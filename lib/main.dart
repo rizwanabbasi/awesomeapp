644b3d111f5ac52e84870c1823ddb6aa1034e9ac
Rizwan Abbasi <rizwan@rizwanabbasi.com>
2022-08-15 15:19:54 +0500

    Chapter 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const HomePage());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +13,37 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Awesome App Title"),
         ),
-        body: const Center(
-          child: Text("Awesome App"),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            width: 150,
+            height: 100,
+            transform: Matrix4.rotationZ(0.05),
+            decoration: BoxDecoration(
+                //shape: BoxShape.circle,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.blueGrey,
+                      blurRadius: 5,
+                      offset: Offset(2.0, 5.0)),
+                ],
+                color: Colors.teal,
+                gradient:
+                    const LinearGradient(colors: [Colors.yellow, Colors.pink])),
+            child: const Text(
+              "Container Widget",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
         ),
       ),
     );
   } //build
-} //MyApp
+} //HomePage
